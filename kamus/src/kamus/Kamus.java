54b1c7206c87d3c7eec5e307f16d8a052926ec2e
fadil <wardi.fadillah@detik.com>
2020-02-18 16:11:09 +0700

    first commit

---
+++ kamus/src/kamus/Kamus.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kamus;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author TOSHIBA
+ */
+public class Kamus {
+
+    private static Connection conn;
+    
+    public static Connection configDB()throws SQLException {
+        
+        try{
+            String url = "jdbc:mysql://localhost/psikologi";
+            String user ="root";
+            String pass ="";
+            
+            DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+            conn = DriverManager.getConnection(url,user,pass);
+            
+            
+        }catch(SQLException e){
+            System.out.println("Koneksi Gagal" + e.getMessage());
+            
+        }
+        return conn;
+    }
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
